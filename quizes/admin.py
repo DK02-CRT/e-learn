6a4eb1c92cd9ae813381c4a670ea1c2fb013aabd
DK02-CRT <25972@edu.amw.gdynia.pl>
2026-06-24 19:24:58 +0200

    fixbug: correcting issues due error with previous merge

--- quizes/admin.py
+++ quizes/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Quiz, Quiz_Task, Quiz_Answer
 
-admin.site.register(Quiz)
-admin.site.register(Quiz_Task)
-admin.site.register(Quiz_Answer)
-# Register your models here.
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+
+@admin.register(Quiz_Task)
+class QuizTaskAdmin(admin.ModelAdmin):
+    search_fields = ["context"]
+
+
+@admin.register(Quiz_Answer)
+class QuizAnswerAdmin(admin.ModelAdmin):
+    search_fields = ["option"]
