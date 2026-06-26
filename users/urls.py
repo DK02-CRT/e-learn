8abb6c0d6008a241733d31cc09011b0185a0258b
DK02-CRT <25972@edu.amw.gdynia.pl>
2026-06-26 12:59:18 +0200

    including singup template

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import signin, acccount, signout
+from .views import signin, acccount, signout, signup
 
 urlpatterns = [
     path('signin/', signin, name='signin'),
     path('account/', acccount, name='account'),
-    path('signout/', signout, name='signout')
+    path('signout/', signout, name='signout'),
+    path('signup/', signup, name='signup'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import datetime
+from users.models import User
 
 def signin (request):
 
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        # print(username)
-        # print(password)
 
         user = authenticate(
             request,
@@ -33,10 +33,13 @@ def acccount(request):
     if request.method == "POST":
         user = request.user
 
-        user.first_name = request.POST.get("first_name")
-        user.last_name = request.POST.get("last_name")
-        user.username = request.POST.get("username")
-        user.email = request.POST.get("email")
+        user.first_name = request.POST.get("first_name") or user.first_name
+        user.last_name = request.POST.get("last_name") or user.last_name
+        user.username = request.POST.get("username") or user.username
+        user.email = request.POST.get("email") or user.email
+        if "avatar" in request.FILES:
+            user.avatar = request.FILES["avatar"]
+        user.modified_at = datetime.datetime.now()
 
         user.save()
 
@@ -44,8 +47,32 @@ def acccount(request):
     
     return render(request, "users/account.html")
 
-
-
 def signout(request):
     logout(request)
     return redirect("home")
+
+def signup(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(
+            first_name = first_name,
+            last_name = last_name,
+            username=username,
+            email=email,
+            password=password
+        )
+
+        user = authenticate(
+            request,
+            username = username,
+            password = password
+        )
+
+        return redirect("home")
+
+    return render(request, "users/signup.html")
