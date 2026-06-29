fb5f635cb921aa612e2283d559d41aa2e8789e25
DK02-CRT <25972@edu.amw.gdynia.pl>
2026-06-29 16:33:48 +0200

    changing structure of models in results/models.py

--- results/models.py
+++ results/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.conf import settings
 from quizes.models import Quiz
+from coureses.models import Topic
 
-class Results(models.Model):
+class ResultsQuiz(models.Model):
     users = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="results"
+        related_name="quiz_results"
     )
     quiz = models.ForeignKey(
         Quiz,
         on_delete=models.CASCADE,
-        related_name='results'
+        related_name='quiz_results'
     )
     score = models.IntegerField(default=0)
     max_score = models.IntegerField(default=0)
 
     started_at = models.DateTimeField()
-    finished_at = models.DateTimeField()
 
     duration = models.DurationField()
 
@@ -29,3 +29,30 @@ class Results(models.Model):
 
     def __str__(self):
         return f"{self.users} - {self.quiz}"
+
+class ResultsTopic(models.Model):
+    users = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="topic_results"
+    )
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name='topic_results'
+    )
+    score = models.IntegerField(default=0)
+    max_score = models.IntegerField(default=0)
+
+    started_at = models.DateTimeField()
+
+    duration = models.DurationField()
+
+    class Meta:
+        ordering = [
+            "-score",
+            "duration"
+        ]
+
+    def __str__(self):
+        return f"{self.users} - {self.topic}"
