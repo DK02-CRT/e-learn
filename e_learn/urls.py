d99f160940fb5c59b8389e57f00f4495631a68ea
DK02-CRT <25972@edu.amw.gdynia.pl>
2026-07-03 00:28:58 +0200

    adding healchecks to containers in docker-compose.yml file

--- e_learn/urls.py
+++ e_learn/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import home
+from .views import home, health_check
 
 urlpatterns = [
     path('', home, name='home'),
     path('courses/', include('coureses.urls')),
+    path('health_check/', health_check, name='health_chech')
 ]
--- e_learn/views.py
+++ e_learn/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Mode
+from django.http import HttpResponse
 
 def home (request):
     modes = Mode.objects.all()
@@ -7,3 +8,6 @@ def home (request):
     return render(request, "e_learn/strona.html", {
         "modes": modes
     })
+
+def health_check (request):
+    return HttpResponse("OK")
